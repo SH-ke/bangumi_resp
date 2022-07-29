4e9b36d5350f96fece9ba9308a94395a4ec67ecd
SH-ke <SHke_dev@163.com>
2022-07-29 20:56:21 +0800

    实现protoc格式解析 perfect!

--- src/danmaku_parse.py
+++ src/danmaku_parse.py
@@ -4,27 +4,24 @@ from dm_pb2 import DmSegMobileReply
 from google.protobuf.json_format import MessageToJson
  
  
-b_web_cookie = 'SESSDATA=fd25e2e6%2C1660373048%2C287c9%2A21;'
- 
- 
-def get_date_list():
-    url = "https://api.bilibili.com/x/v2/dm/history/index?type=1&oid=168855206&month=2022-02"
-    headers = {
-        'cookie': b_web_cookie
-    }
-    response = requests.get(url, headers=headers)
-    print(json.dumps(response.json(), ensure_ascii=False, indent=4))
- 
- 
 def dm_real_time():
-    url_real_time = 'https://api.bilibili.com/x/v2/dm/web/seg.so?type=1&oid=168855206&pid=98919207&segment_index=1'
-    resp = requests.get(url_real_time)
+    params = {
+        "type": 1, 
+        "oid": 168855206, 
+        "pid": 98919207, 
+        "segment_index": 1, 
+        }
+    url_real_time = 'https://api.bilibili.com/x/v2/dm/web/seg.so'
+    resp = requests.get(url_real_time, params=params)
  
-    DM = DmSegMobileReply()
-    DM.ParseFromString(resp.content)
-    data_dict = json.loads(MessageToJson(DM))
-    # print(data_dict)
-    # list(map(lambda x=None: print(x['content']), data_dict.get('elems', [])))
+    DM = DmSegMobileReply() # 导入 protoc 结构体
+    DM.ParseFromString(resp.content) # 解析字符串
+    data_dict = json.loads(MessageToJson(DM)) # 弹幕文本转json
+
+    # 若返回值为空 则 return
+    if not data_dict:
+        print("弹幕分片超出上限")
+        return 
 
     # data_dict["elems"] 数据结构为列表，其中的每个元素是一个字典
     # 字典中存储了每条弹幕的信息，弹幕结构体详见 dm.proto 文件
@@ -32,23 +29,17 @@ def dm_real_time():
     dm_list = data_dict["elems"]
     for k, v in dm_list[0].items():
         print(f"k = {k}, v = {v}")
- 
- 
-def dm_history():
-    url_history = 'https://api.bilibili.com/x/v2/dm/web/history/seg.so?type=1&oid=168855206&date=2022-02-23'
-    headers = {
-        'cookie': b_web_cookie
-    }
-    resp = requests.get(url_history, headers=headers)
-    DM = DmSegMobileReply()
-    DM.ParseFromString(resp.content)
-    data_dict = json.loads(MessageToJson(DM))
-    # print(data_dict)
-    list(map(lambda x=None: print(x['content']), data_dict.get('elems', [])))
+
+    # 保存弹幕文件为json
+    with open("target/dm_sgbl.json", "w", encoding="utf8") as f:
+        json.dump({
+            "elems": dm_list, 
+        }, f, indent=4)
+
+    # 直接保存二进制文件
+    with open("target/dm_sgbl.seg.so", "wb") as f:
+        f.write(resp.content)
  
  
 if __name__ == '__main__':
     dm_real_time()
-    # get_date_list()
-    # dm_history()
-    pass
